423645ea89d3a0dedd538f9ad8ecb683d4d48d03
Mastrippolito Lorenzo <mastrippolito.lorenz@osdbsesto>
2026-04-24 13:57:57 +0200

    [ADD] aggiunto main

--- Esercizi/src/SportelloBancario/Main.java
+++ Esercizi/src/SportelloBancario/Main.java
@@ -1,11 +1,27 @@
 package SportelloBancario;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Sportello sportello = new Sportello();
 
-    public static void main(String[] args) {
+        List<Cliente> clienti = new ArrayList<>();
 
+        for (int i = 0; i < 8; i++) {
+            Cliente c = new Cliente(sportello, "Cliente-" + i);
+            clienti.add(c);
+            c.start();
+        }
 
+        for (Cliente c : clienti) {
+            c.join();
+        }
 
+        System.out.println();
+        System.out.println("--- RISULTATI ---");
+        System.out.println("Serviti: " + Cliente.getServiti());
+        System.out.println("Rinunciati: " + Cliente.getRinunciati());
     }
-
 }
